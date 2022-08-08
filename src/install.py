f1eedb76550408421a1286d57adfe5f949d8ea52
PeakLinux <linuxpeak01@gmail.com>
2022-08-07 21:15:36 -0400

    commit

--- src/install.py
+++ src/install.py
@@ -1,9 +1,16 @@
 from . import parseConfig
+import hashlib
+import wget
 
 class Packages:
 	def __init__(self, packages, config):
 		self.packages = packages
 		self.config = parseConfig.parse(config)
 
+	def search(self):
+		print(f"Searching for: {self.packages}")
+
 	def install(self):
-		print(self.packages)
+		print("Checking if cached repos are up to date")
+		repos = parseConfig.parse
+		wget.download()

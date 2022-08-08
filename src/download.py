b5b3d84c2eb4e58102c66c2d7a70303c64457a2a
PeakLinux <linuxpeak01@gmail.com>
2022-08-07 23:56:42 -0400

    update

--- src/download.py
+++ src/download.py
@@ -1,14 +1,21 @@
 from tqdm import tqdm
 import requests
 import os
+from typing import Optional
 
 class Download:
-	def download(url, outdir):
-		buffer = 1024
+	def download(url, outdir, filename: Optional[str]=None):
+		buffer = 16
 		response = requests.get(url, stream=True)
 
 		downloadSize = int(response.headers.get("Content-Length", 0))
-		filename = os.path.join(outdir, url.split('/')[-1])
+
+		print(url)
+		
+		if not filename:
+			filename = os.path.join(outdir, url.split('/')[-1])
+		else:
+			filename = os.path.join(outdir, filename)
 
 		progress = tqdm(response.iter_content(buffer), f"Updating the {filename.split('/')[-2]} repository", total=downloadSize, unit="B", unit_scale=True, unit_divisor=1024)
 		with open(filename, "wb") as f:
--- src/install.py
+++ src/install.py
@@ -3,6 +3,7 @@ import hashlib
 import wget
 import os
 from . import download
+import sys
 
 class Packages:
 	def __init__(self, packages, config):
@@ -28,15 +29,29 @@ class Packages:
 			# if os.path.isfile('etc/ascent/repos/cache/{foldername}/repo-pub.json') and os.path.isfile('etc/ascent/repos/cache/{foldername}/repo-pub.json.sha256'):
 			# 	download.Download.download(f"{url}.sha256", f'etc/ascent/repos/cache/{foldername}')
 			if os.path.isfile(f'etc/ascent/repos/cache/{foldername}/repo-pub.json') and os.path.isfile(f'etc/ascent/repos/cache/{foldername}/repo-pub.json.sha256'):
-				url = url.split('/')
-				download.Download.download(f"{url}.sha256", f'etc/ascent/repos/cache/{foldername}/{url[-1]}.sha256.tmp')
+				file = url.split('/')[-1]
+				download.Download.download(f"{url}.sha256", f'etc/ascent/repos/cache/{foldername}', f"{file}.sha256.tmp")
 				sha256sum = hashlib.sha256()
-				with open(f"etc/ascent/repos/cache/{foldername}/{url[-1]}", "rb") as f:
+				with open(f"etc/ascent/repos/cache/{foldername}/{file}", "rb") as f:
 					for chunk in iter(lambda: f.read(4096), b""):
 						sha256sum.update(chunk)
-				with open(f"etc/ascent/repos/cache/{foldername}/{url[-1]}.sha256", "w") as f:
-					f.write(sha256sum.hexdigest())
-				# print(hashlib.sha256(f"etc/ascent/repos/cache/{foldername}/{url[:-1]}"))
+				with open(f"etc/ascent/repos/cache/{foldername}/{file}.sha256", "w") as f:
+					localchecksum = sha256sum.hexdigest()
+					f.write(localchecksum)
+				
+				with open(f'etc/ascent/repos/cache/{foldername}/{file}.sha256.tmp', 'r') as f:
+					line = f.read()
+					if line.rstrip() == localchecksum:
+						continue
+					else:
+						updateRepos = str(input("Checksums do not match. Would you like me to update the local repositories for you? (Y/n) "))
+						if updateRepos == '' or updateRepos == 'Y' or updateRepos == 'y':
+							download.Download.download(f"{url}", f'etc/ascent/repos/cache/{foldername}')
+							download.Download.download(f"{url}.sha256", f'etc/ascent/repos/cache/{foldername}')
+						else:
+							print("User chose to not update mirrors. Aborting now.")
+							sys.exit(1)
+
 			else:
 				download.Download.download(f"{url}", f'etc/ascent/repos/cache/{foldername}')
 				download.Download.download(f"{url}.sha256", f'etc/ascent/repos/cache/{foldername}')
